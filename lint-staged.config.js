c17d50c77c9e2d8b0e40cb8f475c09b54ba153f6
Franciszek Orzechowski <orzechowskifranek@gmail.com>
2025-11-21 22:55:48 +0100

    chore(config): set TS moduleResolution to bundler and streamline lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,6 @@
 // lint-staged.config.js
 module.exports = {
-  '**/*': ['next lint .'],
-  '*.{ts,tsx}': [() => 'tsc --noEmit --incremental false'],
   '**/*.{js,jsx,ts,tsx}': ['eslint --max-warnings=0 --no-warn-ignored', 'prettier -w'],
+  '*.{ts,tsx}': [() => 'tsc --noEmit --incremental false'],
   '**/*.{json,css,scss,md}': ['prettier -w'],
 };
